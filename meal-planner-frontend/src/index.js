efce552863548f4d5e211a8f08a480a95ef3169e
rgrier104 <rgrier104@gmail.com>
2019-11-19 17:34:28 -1000

    add event listener to hide and display new recipe form

--- meal-planner-frontend/src/index.js
+++ meal-planner-frontend/src/index.js
@@ -1,27 +1,44 @@
 
 let RECIPES_URL = "http://127.0.0.1:3000/recipes"
 
+
 document.addEventListener("DOMContentLoaded", () => {
+
     fetchRecipes();
-})
+
+    let addRecipe = false;
+    let newRecipeBtn = document.getElementById("new-recipe-button");
+    let recipeForm = document.getElementById("new-recipe-container")
+
+    newRecipeBtn.addEventListener("click", () => {
+        addRecipe = !addRecipe
+        if (addRecipe) {
+            recipeForm.style.display = 'block'
+        } else {
+            recipeForm.style.display = 'none'
+        }
+    })
 
 
-function fetchRecipes() {
-    fetch(RECIPES_URL)
-        .then(resp => resp.json())
-        .then(json => json.forEach(recipe => {
-        renderRecipe(recipe)
-    }))
-}
+    function fetchRecipes() {
+        fetch(RECIPES_URL)
+            .then(resp => resp.json())
+            .then(json => json.forEach(recipe => {
+                renderRecipe(recipe)
+            }))
+    }
 
-function renderRecipe(recipe) {
-    const allRecipesTable = document.getElementById("all recipes");
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
+    function renderRecipe(recipe) {
+        const allRecipesTable = document.getElementById("all-recipes");
+        const tableRow = document.createElement('tr');
+        tableRow.innerHTML = `
         <td>${recipe.name}</td>
         <td>${recipe.meal_type}</td>
         <td>${recipe.cuisine}</td>
     `
-    allRecipesTable.appendChild(tableRow);
+        allRecipesTable.appendChild(tableRow);
 
-}
+    }
+
+})
+
