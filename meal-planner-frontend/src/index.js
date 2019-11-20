1af0ada5c69183124cbecf93916c29e1f2d3094d
rgrier104 <rgrier104@gmail.com>
2019-11-20 12:07:33 -1000

    add event listener to submit new recipe and create new recipe in database

--- meal-planner-frontend/src/index.js
+++ meal-planner-frontend/src/index.js
@@ -32,13 +32,42 @@ document.addEventListener("DOMContentLoaded", () => {
         const allRecipesTable = document.getElementById("all-recipes");
         const tableRow = document.createElement('tr');
         tableRow.innerHTML = `
-        <td>${recipe.name}</td>
+        <td><a href="${recipe.url}">${recipe.name}</a></td>
         <td>${recipe.meal_type}</td>
         <td>${recipe.cuisine}</td>
     `
         allRecipesTable.appendChild(tableRow);
 
     }
+
+    recipeForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        let recipeName = event.target.name.value;
+        let recipeUrl = event.target.url.value;
+        let recipeType = event.target.meal_type.value;
+        let recipeCuisine = event.target.cuisine.value;
+        
+        let formData = {
+            name: recipeName,
+            url: recipeUrl,
+            meal_type: recipeType,
+            cuisine: recipeCuisine
+        };
+
+        let configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(formData)
+        };
+
+        return fetch(RECIPES_URL, configObj) 
+            .then(resp => resp.json())
+            .then(json => renderRecipe(json))
+        
+    })
 
 })
 
