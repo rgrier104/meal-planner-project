9e15ccf1f955779a436f378f641aa4bcf8a3a139
rgrier104 <rgrier104@gmail.com>
2019-11-22 13:15:24 -1000

    fix submit event listener and move displays out of class

--- meal-planner-frontend/src/meal_plan.js
+++ meal-planner-frontend/src/meal_plan.js
@@ -4,38 +4,46 @@ class MealPlan {
         this.notes = notes;
     }
 
-    static displayMealPlan() {
-        let addMealPlan = false;
-        const mealPlanBtn = document.getElementById("new-meal-plan");
-        const mealPlanContainer = document.getElementById("meal-plan-week");
-        mealPlanBtn.addEventListener("submit", (e) => {
-            e.preventDefault();
-            let newMealPlan = new MealPlan();
-            newMealPlan.name = e.target.name.value
-            addMealPlan = !addMealPlan
-            if (addMealPlan) {
-                mealPlanContainer.style.display = 'block'
-            } else {
-                mealPlanContainer.style.display = 'none'
-            }
-        })
-    }
+    
 
-    static renderCalendar() {
-        const mealPlanContainer = document.getElementById("meal-plan-week");
-        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday","Friday", "Saturday"]
-        for (let i = 0; i < 7; i++) {
-            const dayContainer = document.createElement("div");
-            dayContainer.className = "day-card"
-            dayContainer.innerHTML = `
-            <h5>${daysOfWeek[i]}</h5>
-        `
-            mealPlanContainer.appendChild(dayContainer);
-        }
-        
-    }
+    
 
 }
 
-MealPlan.displayMealPlan();
-MealPlan.renderCalendar();
+function displayMealPlan() {
+    let addMealPlan = false;
+    const mealPlanForm = document.getElementById("new-meal-plan");
+    const mealPlanContainer = document.getElementById("meal-plan-week");
+    mealPlanForm.addEventListener("submit", (e) => {
+        console.log("test")
+        e.preventDefault();
+
+        let newMealPlan = new MealPlan();
+        newMealPlan.name = e.target.name.value
+        addMealPlan = !addMealPlan
+        if (addMealPlan) {
+            mealPlanContainer.style.display = 'block'
+        } else {
+            mealPlanContainer.style.display = 'none'
+        }
+    })
+}
+
+function renderCalendar() {
+    const mealPlanContainer = document.getElementById("meal-plan-week");
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    for (let i = 0; i < 7; i++) {
+        const dayContainer = document.createElement("div");
+        dayContainer.className = "day-card"
+        dayContainer.innerHTML = `
+            <h5>${daysOfWeek[i]}</h5>
+            <h3>Breakfast</h3>
+            <h3>Lunch</h3>
+            <h3>Dinner</h3>
+            <h3>Snack</h3>
+            <h3>Other</h3>
+        `
+        mealPlanContainer.appendChild(dayContainer);
+    }
+
+}
