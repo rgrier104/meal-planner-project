fbb58b339819a5f95f2f2630f5121a8291d7288d
rgrier104 <rgrier104@gmail.com>
2019-11-21 17:09:43 -1000

    add function to display meal plan on DOM

--- meal-planner-frontend/src/meal_plan.js
+++ meal-planner-frontend/src/meal_plan.js
@@ -4,5 +4,21 @@ class MealPlan {
         this.notes = notes;
     }
 
+    static displayMealPlan() {
+        let addMealPlan = false;
+        const mealPlanBtn = document.getElementById("new-meal-plan");
+        const mealPlanContainer = document.getElementById("meal-plan-week");
+        mealPlanBtn.addEventListener("click", () => {
+            let newMealPlan = new MealPlan();
+            addMealPlan = !addMealPlan
+            if (addMealPlan) {
+                mealPlanContainer.style.display = 'block'
+            } else {
+                mealPlanContainer.style.display = 'none'
+            }
+        })
+    }
 
 }
+
+MealPlan.displayMealPlan();
