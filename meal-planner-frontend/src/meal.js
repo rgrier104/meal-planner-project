a5e7de9dd0f10c27da20a1953aee6b6e77970019
rgrier104 <rgrier104@gmail.com>
2019-11-22 14:10:37 -1000

    add meal javascript class and file

---
+++ meal-planner-frontend/src/meal.js
@@ -0,0 +1,8 @@
+class Meal {
+    constructor(meal_plan_id, recipe_id, day, meal_type) {
+        this.meal_plan_id = meal_plan_id;
+        this.recipe_id = recipe_id;
+        this.day = day;
+        this.meal_type = meal_type;
+    }
+}
